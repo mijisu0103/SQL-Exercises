d08726695a2af4e9a243fdc01b1831e4447c57a0
Jisu Park <86939562+mijisu0103@users.noreply.github.com>
2024-10-09 21:19:17 +0100

    W3-Ex4

---
+++ W3/W3-Ex4-sqlscript.sql
@@ -0,0 +1,147 @@
+REM   Script: W3 - Exercise 4 SQL
+REM   W3 - Exercise 4 SQL
+
+CREATE TABLE EMP 
+       (EMPNO NUMBER(4) NOT NULL, 
+        ENAME VARCHAR2(10), 
+        JOB VARCHAR2(9), 
+        MGR NUMBER(4), 
+        HIREDATE DATE, 
+        SAL NUMBER(7, 2), 
+        COMM NUMBER(7, 2), 
+        DEPTNO NUMBER(2));
+
+INSERT INTO EMP VALUES 
+        (7369, 'SMITH',  'CLERK',     7902, 
+        TO_DATE('17-DEC-2019', 'DD-MON-YYYY'),  1600, NULL, 20);
+
+INSERT INTO EMP VALUES 
+        (7499, 'ALLEN',  'SALESMAN',  7698, 
+        TO_DATE('20-FEB-2017', 'DD-MON-YYYY'), 3200,  300, 30);
+
+INSERT INTO EMP VALUES 
+        (7521, 'WARD',   'SALESMAN',  7698, 
+        TO_DATE('22-FEB-2018', 'DD-MON-YYYY'), 2500,  500, 30);
+
+INSERT INTO EMP VALUES 
+        (7566, 'JONES',  'MANAGER',   7839, 
+        TO_DATE('2-APR-2014', 'DD-MON-YYYY'),  5000, NULL, 20);
+
+INSERT INTO EMP VALUES 
+        (7654, 'MARTIN', 'SALESMAN',  7698, 
+        TO_DATE('28-SEP-2019', 'DD-MON-YYYY'), 2500, 1400, 30);
+
+INSERT INTO EMP VALUES 
+        (7698, 'BLAKE',  'MANAGER',   7839, 
+        TO_DATE('1-MAY-2015', 'DD-MON-YYYY'),  4500, NULL, 30);
+
+INSERT INTO EMP VALUES 
+        (7782, 'CLARK',  'MANAGER',   7839, 
+        TO_DATE('9-JUN-2017', 'DD-MON-YYYY'),  3000, NULL, 10);
+
+INSERT INTO EMP VALUES 
+        (7788, 'SCOTT',  'ANALYST',   7566, 
+        TO_DATE('09-DEC-2019', 'DD-MON-YYYY'), 4000, NULL, 20);
+
+INSERT INTO EMP VALUES 
+        (7839, 'KING',   'PRESIDENT', NULL, 
+        TO_DATE('17-NOV-2014', 'DD-MON-YYYY'), 6000, NULL, 10);
+
+INSERT INTO EMP VALUES 
+        (7844, 'TURNER', 'SALESMAN',  7698, 
+        TO_DATE('8-SEP-2017', 'DD-MON-YYYY'),  3000,    0, 30);
+
+INSERT INTO EMP VALUES 
+        (7876, 'ADAMS',  'CLERK',     7788, 
+        TO_DATE('12-JAN-2020', 'DD-MON-YYYY'), 2200, NULL, 20);
+
+INSERT INTO EMP VALUES 
+        (7900, 'JAMES',  'CLERK',     7698, 
+        TO_DATE('3-DEC-2019', 'DD-MON-YYYY'),   2000, NULL, 30);
+
+INSERT INTO EMP VALUES 
+        (7902, 'FORD',   'ANALYST',   7566, 
+        TO_DATE('3-DEC-2018', 'DD-MON-YYYY'),  4000, NULL, 20);
+
+INSERT INTO EMP VALUES 
+        (7934, 'MILLER', 'CLERK',     7782, 
+        TO_DATE('23-JAN-2020', 'DD-MON-YYYY'), 2600, NULL, 10);
+
+CREATE TABLE DEPT 
+       (DEPTNO NUMBER(2), 
+        DNAME VARCHAR2(14), 
+        LOC VARCHAR2(13) );
+
+INSERT INTO DEPT VALUES (10, 'ACCOUNTING', 'NEW YORK');
+
+INSERT INTO DEPT VALUES (20, 'RESEARCH',   'DALLAS');
+
+INSERT INTO DEPT VALUES (30, 'SALES',      'CHICAGO');
+
+INSERT INTO DEPT VALUES (40, 'OPERATIONS', 'BOSTON');
+
+CREATE TABLE BONUS 
+        (ENAME VARCHAR2(10), 
+         JOB   VARCHAR2(9), 
+         SAL   NUMBER, 
+         COMM  NUMBER);
+
+CREATE TABLE SALGRADE 
+        (GRADE NUMBER, 
+         LOSAL NUMBER, 
+         HISAL NUMBER);
+
+INSERT INTO SALGRADE VALUES (1,  1000, 2500);
+
+INSERT INTO SALGRADE VALUES (2, 1200, 3000);
+
+INSERT INTO SALGRADE VALUES (3, 1500, 3000);
+
+INSERT INTO SALGRADE VALUES (4, 2001, 5200);
+
+INSERT INTO SALGRADE VALUES (5, 4001, 9999);
+
+CREATE TABLE DUMMY 
+        (DUMMY NUMBER);
+
+INSERT INTO DUMMY VALUES (0);
+
+COMMIT;
+
+-- 1) Find the name and salary of employees in Dallas
+SELECT ENAME, SAL FROM EMP 
+JOIN DEPT ON EMP.DEPTNO = DEPT.DEPTNO 
+WHERE DEPT.LOC = 'DALLAS';
+
+-- 2) Join the dept table to the emp table and show in department number order
+
+SELECT EMP.EMPNO, EMP.ENAME, EMP.JOB, EMP.SAL, DEPT.DNAME, DEPT.LOC 
+FROM EMP 
+JOIN DEPT ON EMP.DEPTNO = DEPT.DEPTNO 
+ORDER BY DEPT.DEPTNO;
+
+-- 3) List all departments that have employees plus those departments that do not have employees
+
+SELECT DEPT.DEPTNO, DEPT.DNAME, DEPT.LOC, EMP.ENAME 
+FROM DEPT 
+LEFT JOIN EMP ON DEPT.DEPTNO = EMP.DEPTNO 
+ORDER BY DEPT.DEPTNO;
+
+-- 4) List all departments that do not have any employees
+SELECT DEPT.DEPTNO, DEPT.DNAME, DEPT.LOC FROM DEPT 
+LEFT JOIN EMP ON DEPT.DEPTNO = EMP.DEPTNO 
+WHERE EMP.EMPNO IS NULL;
+
+-- 5) Find all the employees that earn more than jones Using temporary labels to abbreviate table names
+
+SELECT E.ENAME, E.SAL FROM EMP E 
+WHERE E.SAL > (SELECT SAL FROM EMP WHERE ENAME = 'JONES');
+
+-- 6) For each employee whose salary exceeds his manager's salary list the employee's name and a salary and the manager's name and salary
+
+SELECT E.ENAME AS E_Name, E.SAL AS E_Sal, 
+       M.ENAME AS M_Name, M.SAL AS M_Sal 
+FROM EMP E 
+JOIN EMP M ON E.MGR = M.EMPNO 
+WHERE E.SAL > M.SAL;
+
